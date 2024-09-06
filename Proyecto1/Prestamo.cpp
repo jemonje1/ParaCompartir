2d4d5061a594dcce5e7757f7bb3a4a631c744672
jemonje1 <157499614+jemonje1@users.noreply.github.com>
2024-09-05 20:47:26 -0600

    Delete Proyecto1 directory

--- Proyecto1/Prestamo.cpp
+++
@@ -1,7 +0,0 @@
-#include "Prestamo.h"
-
-Prestamo::Prestamo(string cliente, string banco, string tipo, double cantidad, time_t fecha)
-    : nombreCliente(cliente), nombreBanco(banco), tipoPrestamo(tipo), cantidadInicial(cantidad), fechaInicio(fecha), interes(0.0) {}
-
-Prestamo::~Prestamo() {
-}
